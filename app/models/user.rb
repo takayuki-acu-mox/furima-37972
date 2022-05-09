ef65910b02066b7dcf3aea07fc31ff059ba9bab3
takayuki-acu.mox <taka.harikyu@gmail.com>
2022-05-09 17:26:58 +0900

    ログイン・新規登録・ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :last_name, 
+         format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/},
+         presence: true
+         validates :first_name,
+         format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/},
+         presence: true
+         validates :last_name_k, 
+         format: { with: /\A[ァ-ヶー－]+\z/},
+         presence: true
+         validates :first_name_k, 
+         format: { with: /\A[ァ-ヶー－]+\z/},
+         presence: true
+         validates :birthday, presence: true
+
 end
