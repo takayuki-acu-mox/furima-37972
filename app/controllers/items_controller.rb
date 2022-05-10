f86a31607429f42e80c06ef534a93b4c06754b1f
takayuki-acu.mox <taka.harikyu@gmail.com>
2022-05-10 21:48:36 +0900

    ActiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,10 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
-    # @item = Item.new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :text, :category_id, :product_condition_id, :shipping_fee_id, :shipment_area_id, :shipping_date_id, :price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :product_condition
+  belongs_to :shipping_fee
+  belongs_to :shipment_area
+  belongs_to :shipping_date
   belongs_to :user
+
+  validates :nickname, presence: true, length: { maximum: 40 }  
+  validates :text, presence: true
+  validates :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :product_condition_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :shipment_area_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :shipping_date_id, numericality: { other_than: 1, message: "can't be blank"} 
+
 end
