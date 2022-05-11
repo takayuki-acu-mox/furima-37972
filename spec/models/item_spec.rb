24e1e4d0586a2b47a0042b4bb023c9aa00bd91c3
takayuki-acu.mox <taka.harikyu@gmail.com>
2022-05-11 17:25:34 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -65,28 +65,35 @@ RSpec.describe Item, type: :model do
       it 'カテゴリーに「---」が選択されている場合は出品できない' do
         @item.category_id = 1
         @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+
       end
 
     
       it '商品の状態に「---」が選択されている場合は出品できない' do
         @item.product_condition_id = 1
         @item.valid?
+        expect(@item.errors.full_messages).to include("Product condition can't be blank")
+
       end
 
       it '配送料の負担に「---」が選択されている場合は出品できない' do
         @item.shipping_fee_id = 1
         @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
       end
 
     
       it '発送元の地域に「---」が選択されている場合は出品できない' do
         @item.shipment_area_id = 1
         @item.valid?
+        expect(@item.errors.full_messages).to include("Shipment area can't be blank")
       end
 
       it '発送までの日数に「---」が選択されている場合は出品できない' do
         @item.shipping_date_id = 1
         @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping date can't be blank")
       end
 
       it 'priceが空だと出品できない' do
