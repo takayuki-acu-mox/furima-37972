139620ade0bfa9fbfc1c24972521bbed7ab1b8ca
takayuki-acu.mox <taka.harikyu@gmail.com>
2022-05-18 15:23:03 +0900

    修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,7 +9,7 @@ class OrderAddress
     validates :state_id,numericality: { other_than: 1, message: "can't be blank"}
     validates :city
     validates :house_number
-    validates :phone_number, format: {with: /\A[0-9]{11}\z/ }
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/ }
     validates :token
   end
 
